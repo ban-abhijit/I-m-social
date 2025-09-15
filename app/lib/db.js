c7f1cf362dd4d29e8b415a49a3fdbb934cb904f8
Abhijit Banerjee <abhi26012024@gmail.com>
2025-09-15 21:33:03 +0530

    prisma problem

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,5 +1,3 @@
-import { PrismaClient } from "@prisma/client";
-
-// import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../generated/prisma";
 
 export const prismaC = new PrismaClient();
