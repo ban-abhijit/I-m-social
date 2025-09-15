ec3052be566617e76c96495a478931f9a9d372be
Abhijit Banerjee <abhi26012024@gmail.com>
2025-09-15 21:07:15 +0530

    Added prisma generate postinstall && Added Prisma schema

--- app/lib/db.js
+++ app/lib/db.js
@@ -1,5 +1,5 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-import { PrismaClient } from "../generated/prisma";
+// import { PrismaClient } from "../generated/prisma";
 
 export const prismaC = new PrismaClient();
